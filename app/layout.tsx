9669b3538d37aca47e43c0d5e775ee1be3ff8525
Paul Irungu <paul.irungu@gmail.com>
2024-01-20 15:46:30 +0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import NavBar from "@/components/Navbar";
+import "./globals.css";
+import Footer from "@/components/Footer";
+
+export const metadata = {
+  title: "Car Hub",
+  description: "Discover world's best car showcase application",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="relative">
+        <NavBar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ components/index.ts
@@ -0,0 +1,20 @@
+import CarCard from "./CarCard";
+
+import CustomFilter from "./CustomFilter";
+import Footer from "./Footer";
+import NavBar from "./Navbar";
+import ShowMore from "./ShowMore";
+import SearchBar from "./Searchbar";
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+
+export {
+  Hero,
+  CarCard,
+  CustomButton,
+  CustomFilter,
+  Footer,
+  NavBar,
+  ShowMore,
+  SearchBar,
+};
